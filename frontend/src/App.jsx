acaa6765cde92566263351fd0b65cc83f1df6866
Gihan Sanjeewa <123794603+GihanSanjeewa@users.noreply.github.com>
2024-10-20 01:37:42 +0530

    settings update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,21 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
-import SignUp from './pages/SignUp';
-import SignIn from './pages/SignIn';
-import Dashboard from './pages/Dashboard';
 import Header from './Components/Header';
+import Dashboard from './pages/Dashboard';
+import Signin from './pages/SignIn';
+import Signup from './pages/SignUp';
+import About from './pages/About';
+import Home from './pages/Home';
 
 export default function App() {
   return (
     <BrowserRouter>
     <Header/>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/" element={<Home/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/sign-up" element={<Signup />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path='./dashbord' element={<Dashboard />}/>
       </Routes>
     </BrowserRouter>
   );
