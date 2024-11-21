94db9c8250d339e7cc37dce740b8d6beca0426ff
Gihan Sanjeewa <123794603+GihanSanjeewa@users.noreply.github.com>
2024-11-22 00:31:16 +0530

    complete signin page functions

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -1,7 +1,95 @@
-import React from 'react'
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
-export default function Signin() {
+export default function SignIn() {
+  const [formData, setFormData] = useState({}); //this is for store the from data 
+  const [errorMessage , setErrorMessage] = useState(null); //this is for set the error messages 
+  const [loading, setLoading] = useState(false); // this is for set the loading screen 
+  const navigate = useNavigate();
+
+  const handleChange =(e) => {
+    setFormData({...formData,[e.target.id]: e.target.value.trim()});
+  };
+  const handleSubmit = async (e)=>{
+    e.preventDefault();
+    if(!formData.email || !formData.password){
+      return setErrorMessage('Please Fill all the fields');
+    }
+    try{
+      setLoading(true);
+      setErrorMessage(null);
+      const res = await fetch('/api/auth/SignIn',{
+        method : 'POST',
+        headers : {'Content-Type': 'application/json'},
+        body : JSON.stringify(formData),
+      });
+
+      const data = await res.json();
+
+      if(data.success === false){
+        return setErrorMessage(data.message);
+      }
+
+      setLoading(false);
+
+      if(res.ok){
+        navigate('/');
+      }
+    }catch(error){
+      setLoading(false);
+      setErrorMessage(error.message); 
+    }
+
+
+  };
   return (
-    <div>Signin</div>
+    <div className="min-h-screen mt-20">
+      <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
+        {/* left  */}
+        <div className="flex-1">
+        <Link to="/" className='font-bold dark:text-white text-4xl'> 
+        <span className='px-2 py-1 bg-gradient-to-r from-indigo-500
+         via-purple-500 to-pink-500 rounded-lg text-white'>Gihan</span> Blog
+        </Link>
+        </div>
+        {/* right  */}
+        <div className="flex-1">
+          <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+            <div >
+              <Label value="Your Email" />
+              <TextInput type="email" placeholder="name@company.com" onChange={handleChange} id="email"/>
+            </div>
+            <div >
+              <Label value="Youer Password" />
+              <TextInput type="password" placeholder="*********" onChange={handleChange} id="password"/>
+            </div>
+
+            <Button gradientDuoTone="purpleToPink" type="submit" disabled={loading}>
+              {
+                loading ? (
+                  <>
+                    <Spinner size='sm' />
+                    <span className="pl-3">Loading..</span>
+                  </>
+                ) : 'Sign In'   
+              }
+            </Button>
+          </form>
+          <div className="flex gap-2 text-sm mt-5">
+            <span>Dont Have an account ?</span>
+            <Link to='/sign-up' className="text-blue-500">Sign up</Link>
+          </div>
+          {
+            errorMessage && (
+              <Alert className="mt-4" color="failure">
+                {errorMessage}
+              </Alert>
+            )
+          }
+        </div>
+        
+      </div>
+    </div>
   )
 }
