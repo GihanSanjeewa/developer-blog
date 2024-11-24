22ba9e6f6d373784f165808b4c5f885aa7b8756c
Gihan Sanjeewa <123794603+GihanSanjeewa@users.noreply.github.com>
2024-11-24 17:42:07 +0530

    complete dark mode functionality

--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -2,13 +2,17 @@
 import React from 'react'
 import { Link , useLocation} from 'react-router-dom'
 import { AiOutlineSearch } from 'react-icons/ai'
-import {FaMoon} from 'react-icons/fa';
+import {FaMoon, FaSun} from 'react-icons/fa';
 import { Avatar, Button, Dropdown, Navbar, TextInput } from 'flowbite-react'
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { toggleTheme } from '../redux/theme/themeSlice';
+
 
 export default function Header() {
   const path = useLocation().pathname;
+  const dispatch = useDispatch();
   const {currentUser} = useSelector(state => state.user)
+  const {theme} = useSelector((state) => state.theme);
   return (
     <Navbar clasName="border-b-2">
         <Link to="/" className='self-center whitespace-nowrap text-sm sm:text-xl 
@@ -27,8 +31,8 @@ export default function Header() {
         </Button>
 
         <div className='flex gap-2 md:order-2'>
-          <Button clasName='w-12 h-10 hidden sm:inline' color='gray' pill>
-            <FaMoon/>
+          <Button clasName='w-12 h-10 hidden sm:inline' color='gray' pill onClick={() => dispatch(toggleTheme())}>
+            {theme === 'dark' ? <FaMoon/> : <FaSun/>}
           </Button>
           {currentUser ? (
             <Dropdown arrowIcon={false} inline label={
